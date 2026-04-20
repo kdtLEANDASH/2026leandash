33ee487e3cdd5fca5b4659613c8fd8c76cf45e2f
izzyon0121 <noah4144@naver.com>
2026-04-20 18:35:21 +0900

    레포지토리 초기 설정

---
+++ backend/src/main/java/com/onlyman/leandash/LeandashApplication.java
@@ -0,0 +1,13 @@
+package com.onlyman.leandash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeandashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeandashApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/onlyman/leandash/LeandashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlyman.leandash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeandashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
